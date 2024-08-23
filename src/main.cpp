b7c95c476d07a035566a3ea82291450fae700ab9
iiaypma228 <loolpip586@gmail.com>
2024-08-23 20:21:53 +0300

    delete qt

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
-#include <QCoreApplication>
-#include <QDebug>
+#include<iostream>
+
 
 int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
-    qDebug() << "Hello World";
-    return QCoreApplication::exec();
+
+    std::cout << "Hello world!" << std::endl;
+
+    return 0;
 }
